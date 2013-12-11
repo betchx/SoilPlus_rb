6fa48c5826593c83cf85fcf7076f12ca93d01bf4
Atsushi TANABE <tanabe.atsushi@nikken.jp>
2013-12-11 22:11:54 +0900

    拡張子リストを修正しやすい様にbatの中身を若干整理

--- bin/sp_remover.bat
+++ bin/sp_remover.bat
@@ -1,14 +1,15 @@
 @echo off
 
-rem set TARGET_EXT=err f21 f22 f26 f27 f33 f34 f42 f43 f50 f51 f52 f53 f54 f55 f56 f57 f58 f59 f62 f63 f70 f81 f82 f89
+rem 削除対象となるファイルの拡張子一覧を設定 (次の行を編集）
+set     TARGET_EXT=err     f22 f26 f27 f33 f34 f42 f43 f50 f51 f52 f53 f54 f55 f56 f57 f58 f59 f62 f63 f70         f89
+rem 削除候補拡張子 err f21 f22 f26 f27 f33 f34 f42 f43 f50 f51 f52 f53 f54 f55 f56 f57 f58 f59 f62 f63 f70 f81 f82 f89
 
 rem 後処理で有用なファイルは削除しない様に変更  (2013/12/11)
 rem f21 グラフツールで取得する波形の元データ． rubyからの変換が可能なため，残しておく．
 rem f81 f82 : リスト編集プログラムで参照する場合に必要．
-set     TARGET_EXT=err     f22 f26 f27 f33 f34 f42 f43 f50 f51 f52 f53 f54 f55 f56 f57 f58 f59 f62 f63 f70         f89
+
 
 set BASE=%~dpn1
-
 
 IF "%~x1"==".err" (if NOT "%~z1"=="0" goto :edit_err )
 
