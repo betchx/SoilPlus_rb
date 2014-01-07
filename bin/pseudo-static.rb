53252c80f9a886041a423be95338fd3d8e496269
TANABE Atsushi <tanabe.atsushi@nikken.jp>
2014-01-07 20:03:51 +0900

    動解のdatファイルから擬似静的解析用ファイルを作成する．
    具体的には，
    * 密度を小さく
    * 質点を削除
    の２点を実施．
    
    ただし，微細な振動により大きな加速度が発生するので，
    レイリー減衰を10倍にしている．

---
+++ bin/pseudo-static.rb
@@ -0,0 +1,36 @@
+#! /usr/bin/ruby
+# coding: sjis
+
+ARGV.each do |file|
+  next unless file =~ /\.dat$/i
+  out_file = file.sub(/\.dat$/i, '_ps.dat')
+  open(out_file, "wb") do |out|
+    out.puts "*" * 80
+    out.puts "* Pseudo Static Analysis by #{$0}"
+    out.puts "*  Dynamic analysis with very small density"
+    out.puts "*" * 80
+    open(file, "rb") do |f|
+      while line = f.gets
+        case line
+        when /^MAT /
+          line[-10..-3] = "  0.0001"
+          out.puts line
+        when /^PDMP/
+          #out.puts "**   " + line
+          #out.puts "PDMP         0.0     0.0"
+          #out.puts line
+          b,a = line.split[1,2].map{|x| x.sub(/(\d)([-+]\d)/,'\1E\2').to_f * 10.0}
+          out.puts sprintf("PDMP    %8g%8g",b,a)
+        when /^CMAS/
+          # skip 
+        else
+          out.puts line
+        end
+      end
+    end
+  end
+end
+
+
+
+
